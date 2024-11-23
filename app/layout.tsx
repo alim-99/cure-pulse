86981a7dda6613ae20c1841653df6c51d0b0ca19
alim-99 <alim16097side@gmail.com>
2024-11-24 00:31:54 +0200

    "finished ui of onboarding page"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from '@/components/Theme'
 
 const FontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -12,20 +13,29 @@ const FontSans = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "CarePulse",
   description: "A healthcare managment system",
+  icons: {
+  icon: './logo-icon.svg'
+  }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: 
+  Readonly<{
+    children: React.ReactNode
+  }>
+) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn('min-h-screen bg-dark-300 font-sans antialiased', FontSans.variable)}
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          FontSans.variable
+        )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
+
---
+++ components/PatientForm.tsx
@@ -0,0 +1,91 @@
+"use client"
+import React, { useState } from 'react'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { UserFormValidation } from '@/lib/validation'
+import Image from 'next/image'
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomFormField, { FormFieldType } from './CustomFormField'
+import SubmitButton from './SubmitButton'
+import { useRouter } from 'next/navigation'
+
+
+const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+      resolver: zodResolver(UserFormValidation),
+      defaultValues: {
+        name: "",
+        email: "",
+        phone: ""
+      },
+    })
+  
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+      setIsLoading(true)
+
+      try {
+        const userData = {name, email, phone};
+
+        // const user = await createUser(userData);
+
+        // if(user) router.push('/patients/${user.id}/register')
+      } catch(error) {
+        console.error(error);
+      }
+    }
+
+  return (
+    <Form {...form}>
+
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex-1">
+        <CustomFormField 
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="name"
+        label="Full name"
+        placeholder="John Doe"
+        iconSrc="/user.svg"
+        iconAlt="user"
+        />
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="email"
+        label="Email Address"
+        placeholder="example@.com"
+        iconSrc="/email.svg"
+        iconAlt="user"
+        />
+        <CustomFormField
+        fieldType={FormFieldType.PHONE_INPUT}
+        control={form.control}
+        name="phone"
+        label="Phone Number"
+        placeholder="+00 0342 0453 34"
+        iconSrc="/icon.svg"
+        iconAlt="user"
+        />
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  )
+}
+
+export default PatientForm
